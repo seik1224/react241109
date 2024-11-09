f3e3ea8f112fa5d7e9f087cd277092a7bf1a55e8
seik <seik1224@naver.com>
2024-11-09 15:01:05 +0900

    241109

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Step01 from './step/step01';
+import Step02 from './step/step02';
 
 function App() {
   return (
     <>
       {/* 컴포넌트 : 재사용 가능한 코드 조각(버튼, 텍스트, 이미지 등등...) */}
-      <Comp />
-      <Comp />
-      <Step01 />
+      {/* <Comp /> */}
+      {/* <Comp /> */}
+      {/* <Step01 /> */}
+      <Step02 />
     </>
   );
 }
---
+++ src/step/step02.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styleName from './step.module.css'; // 모듈 사용 : 이름 작명
+
+const Step02 = () => {
+    const blue = 'blue';
+
+  // CSS 작성법
+  return (
+    <>
+        {/* 1. 인라인방식 : camelCase 사용 */}
+        <div style={{color:'red', backgroundColor : 'blue'}}>인라인방식</div>
+        {/* 2. className 사용 */}
+        <div className='blue'>App.css에 스타일 작성해주세요.</div>
+        <div className={blue}>변수 사용한 CSS</div>
+        {/* 3. CSS모듈 사용 */}
+        <div className={styleName.red}>모듈 사용</div>
+        {/* 4. Tailwind CSS 사용  */}
+        <div className='bg-red-500'>Tailwind CSS 사용</div>
+    </>
+  )
+}
+
+export default Step02
